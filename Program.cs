b307596ab6ee7c2716557a9a66a423336240e03c
Mardson <mardson@gmail.com>
2025-10-28 09:47:43 -0300

    Criando os primeiros códigos

---
+++ Program.cs
@@ -0,0 +1,12 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+app.UseStaticFiles();
+
+app.MapControllerRoute(name: "default", pattern: "{controller}/{action}/");
+
+app.Run();
