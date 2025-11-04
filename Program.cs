f5d696e41a10373a37ad696e438197acab5f36d2
Mardson <mardson@gmail.com>
2025-11-04 08:51:24 -0300

    Começando o projeto

---
+++ Program.cs
@@ -0,0 +1,10 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+app.MapControllers();
+app.UseAuthorization();
+app.Run();
